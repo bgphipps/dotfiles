dd9261a687f690f2cc3f7ba8a7e5de6f23b6fa4c
Brian Phipps <bgphipps@gmail.com>
2020-02-04 17:04:55 -0800

    Tfswitch from brew; updating functions to reference k alias; updating paths for fanatics specific go private modules

--- functions.zsh
+++ functions.zsh
@@ -1,6 +1,6 @@
 function ccpauth(){
-  echo "fanauth aws --envGroup $@ --writeCreds; kubernatics login -g $@;"
-  fanauth aws --envGroup $@ --writeCreds; kubernatics login -g $@;
+  echo "fanauth aws --envGroup $@ --writeCreds; k login -g $@;"
+  fanauth aws --envGroup $@ --writeCreds; k login -g $@;
 }
 function dockerstoprm(){
   docker stop "$@" && docker rm -v "$@"
